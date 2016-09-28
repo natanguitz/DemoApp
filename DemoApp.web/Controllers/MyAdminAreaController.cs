6291b619f6df79472e22acff65fb625cc2bc3ffb
alexg <alexg@LAPTOP>
2016-09-29 00:29:20 +0200

    added new admin controller

---
+++ DemoApp.web/Controllers/MyAdminAreaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoApp.web.Controllers
+{
+    public class MyAdminAreaController : Controller
+    {
+        // GET: MyAdminArea
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
